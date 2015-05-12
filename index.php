c41af4218d8a5069121c5620adebf867a0c4cf65
paulrduffy <paulrduffy@gmail.com>
2015-05-12 13:56:34 -0700

    Update index.php
    Change 20150512 - 1356

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <body bgcolor="#0033CC">
         <div class="container">
             <div class="hero-unit">
-                <h1><font face="verdana" color="yellow">Simple PHP Test Application - 209</font></h1>
+                <h1><font face="verdana" color="yellow">Simple PHP Test Application - 504</font></h1>
                 <br>
                 <br>
                 <p><font face="verdana" color="yellow" size="5">This simple PHP test application was deployed in a Runnable Sandbox!</font></p>
