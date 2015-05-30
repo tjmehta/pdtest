701a3e8a460b9bef83b9f566b25ad5804d32973f
paulrduffy <paulrduffy@gmail.com>
2015-05-30 13:18:26 -0700

    Changed color

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-        <!!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
 
     <body bgcolor="#FF0000">
