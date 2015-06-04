135da315a688c14c14f85b4bca7cd3bffd5c21d7
paulrduffy <paulrduffy@gmail.com>
2015-06-04 10:39:14 -0700

    Color change

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
 
-    <body bgcolor="#000000">
+    <body bgcolor="#0000FF">
         <div class="container">
             <div class="hero-unit">
                 <h1><font face="verdana" color="yellow">Simple PHP Test Application</font></h1>
