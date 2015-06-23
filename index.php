021b2bb647db1ec56a814c7f07e8fa8358e9ad16
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-06-22 18:09:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <body bgcolor="#00FF99">
         <div class="container">
             <div class="hero-unit">
-                <h1><font face="verdana" color="yellow">Simple PHP12321312 Test Application</font></h1>
+                <h1><font face="verdana" color="yellow">Simple PHP Test Application</font></h1>
                 <br>
                 <br>
                 <p><font face="verdana" color="yellow" size="5">This simple PHP test application was deployed in a Runnable Sandbox!</font></p>
